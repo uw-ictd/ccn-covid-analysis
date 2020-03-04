1bb0993578e71593b5d93aa7d92dea09456bab62
Jenny Liang <jliang9@uw.edu>
2020-03-04 01:42:27 -0800

    Added code to make a rolling window

--- revenue_over_time.py
+++ revenue_over_time.py
@@ -19,6 +19,10 @@ max_date = datetime.datetime.strptime('2020-02-13 21:29:54', '%Y-%m-%d %H:%M:%S'
 def get_month_year(x):
     return x["start"].apply(lambda x_1: datetime.datetime(year=x_1.year, month=x_1.month, day=1), meta=('start', 'datetime64[ns]'))
 
+
+def get_date(x):
+    return x["start"].apply(lambda x_1: datetime.datetime(year=x_1.year, month=x_1.month, day=x_1.day), meta=('start', 'datetime64[ns]'))
+
 def get_revenue_query(transactions):
     # Set down the types for the dataframe
     types = {
@@ -29,13 +33,17 @@ def get_revenue_query(transactions):
     # Update the types in the dataframe
     query = transactions.astype(types)
     # Map each start to a cohort
-    query = query.assign(month_year=get_month_year)
+    query = query.assign(date=get_date)
     # Group by cohorts and get the all the users
-    query = query.groupby("month_year")["price"]
+    query = query.groupby("date")["price"]
     # Count the number of unique users per cohort
     query = query.sum()
-    # Reverse the array and ignore cohorts that are past the max date
+    # Convert query back into a dataframe
     query = query.reset_index()
+    # Do a rolling average of the days
+    query["rolling_avg"] = query.iloc[:,1].rolling(window=30).mean()
+    # Drop all NA values
+    query = query.dropna(how="any")
 
     return query
 
@@ -56,10 +64,10 @@ if __name__ == "__main__":
     # Get the user data
     revenue = get_revenue_query(transactions)
     # Get the data in a form that is easily plottable
-    revenue = revenue.melt(id_vars=["month_year"], value_vars=["price"], var_name="time", value_name="revenue (rupiah)")
+    revenue = revenue.melt(id_vars=["date"], value_vars=["rolling_avg"], var_name="time", value_name="revenue (rupiah)")
     # Reset the types of the dataframe
     types = {
-        "month_year": "datetime64",
+        "date": "datetime64",
         "revenue (rupiah)": "int64"
     }
     revenue = revenue.astype(types)
@@ -68,7 +76,7 @@ if __name__ == "__main__":
     print(revenue)
 
     altair.Chart(revenue).mark_line().encode(
-        x="month_year:T",
+        x="date:T",
         y="revenue (rupiah)",
     ).serve()
     
