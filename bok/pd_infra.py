b68a4999fbbaa9713d13356e728666d1a6e4c94d
Matt Johnson <matt9j@cs.washington.edu>
2020-05-14 11:21:40 -0700

    Create a shared config module for pandas also

---
+++ bok/pd_infra.py
@@ -0,0 +1,37 @@
+"""Methods pandas manipulations with shared parameters across the project
+"""
+
+import pandas as pd
+import shutil
+
+
+def clean_write_parquet(dataframe, path):
+    """Write a parquet file with common options standardized in the project
+    """
+
+    # Ensure we are writing pandas dataframes, not accidentally dask!
+    if not isinstance(pd.DataFrame, dataframe):
+        raise ValueError(
+            "Attempted to write dask dataframe, but got a {}".format(
+                str(type(dataframe))
+            )
+        )
+
+    # Clear the dest directory to prevent partial mixing of files from an old
+    # archive if the number of partitions has changed.
+    try:
+        shutil.rmtree(path)
+    except FileNotFoundError:
+        # No worries if the output doesn't exist yet.
+        pass
+
+    # Do the write with the given options.
+    return dataframe.to_parquet(path,
+                                compression="snappy",
+                                engine="fastparquet")
+
+
+def read_parquet(path):
+    """Read a parquet file with common options standardized in the project
+    """
+    return pd.read_parquet(path, engine="fastparquet")
