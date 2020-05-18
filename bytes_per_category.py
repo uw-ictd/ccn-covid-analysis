3f6da4886465e3eb83921d65ca76ef070d68a1b0
Matt Johnson <matt9j@cs.washington.edu>
2020-05-17 20:23:24 -0700

    Initial attempt at bytes per category render

--- bytes_per_category.py
+++ bytes_per_category.py
@@ -1,49 +1,89 @@
 """ Computing active and registered users on the network over time
 """
 
-import altair
+import altair as alt
+import pandas as pd
+
 import bok.dask_infra
-import dask.dataframe
-import dask.distributed
+import bok.pd_infra
 
 
-def get_sites_visited_query(data):
-    # Get the date column back
-    query = data.reset_index()
-    # Group by the number of times each site was visited
-    query = query.groupby("category").count()
-    # Create a frequency column
-    query["frequency"] = query["user"]
-    return query
+def reduce_to_pandas(outfile, dask_client):
+    flows = bok.dask_infra.read_parquet(
+        "data/clean/flows/typical_fqdn_category_local_TM_DIV_none_INDEX_start")[["category", "bytes_up", "bytes_down"]]
+
+    # Compress to days
+    flows = flows.reset_index()
+    flows["start_bin"] = flows["start"].dt.floor("d")
+    flows = flows.set_index("start_bin")
+
+    # Do the grouping
+    flows = flows.groupby(["start_bin", "category"]).sum()
+    flows = flows.compute()
+
+    bok.pd_infra.clean_write_parquet(flows, outfile)
+
+
+def make_plot(infile):
+    grouped_flows = bok.pd_infra.read_parquet(infile)
+    grouped_flows = grouped_flows.reset_index()
+    grouped_flows["bytes_total"] = grouped_flows["bytes_up"] + grouped_flows["bytes_down"]
+
+    test = grouped_flows
+    test = grouped_flows.loc[grouped_flows["category"] == None].groupby("fqdn").sum()
+
+    print(test.sort_values("bytes_total"))
+
+    # Consolidate by week instead of by day
+    grouped_flows = grouped_flows[["start_bin", "bytes_total", "category"]].groupby([pd.Grouper(key="start_bin", freq="W-MON"), "category"]).sum()
+
+    grouped_flows = grouped_flows.reset_index()
+
+    print(grouped_flows)
+
+    grouped_flows["GB"] = grouped_flows["bytes_total"] / (1000**3)
+    alt.Chart(grouped_flows).mark_area().encode(
+        x=alt.X("start_bin:T",
+                title="Time",
+                axis=alt.Axis(labels=True),
+                ),
+        y=alt.Y("sum(GB):Q",
+                title="Fraction of Traffic Per Week(GB)",
+                stack="normalize",
+                ),
+        # shape="direction",
+        color="category",
+        detail="category",
+    ).properties(
+        # title="Local Service Use",
+        width=500,
+    ).interactive().serve(port=8085, open_browser=False)
+
+    # .save("renders/bytes_per_category.png",
+    #    scale_factor=2
+    #    )
+
+    # alt.Chart(grouped_flows).mark_area().encode(
+    #     x=alt.X("start_bin:T",
+    #             title="Time",
+    #             axis=alt.Axis(labels=True),
+    #             ),
+    #     y=alt.Y("sum(GB):Q",
+    #             title="Total Traffic Per Week(GB)",
+    #             ),
+    #     # shape="direction",
+    #     color="name",
+    #     detail="name",
+    # ).properties(
+    #     # title="Local Service Use",
+    #     width=500,
+    # ).save(
+    #        scale_factor=2
+    #        )
 
 
 if __name__ == "__main__":
     client = bok.dask_infra.setup_dask_client()
-
-    # Import the dataset
-    #
-    # Importantly, dask is lazy and doesn't actually import the whole thing,
-    # but just keeps track of where the file shards live on disk.
-    data = dask.dataframe.read_parquet("data/clean/flows/typical_with_fqdn_category", engine="fastparquet")
-    print("To see execution status, check out the dask status page at localhost:8787 while the computation is running.")
-    # length = len(data)
-    # print("Processing {} flows".format(length))
-
-    sites_visited = get_sites_visited_query(data)
-
-    # Checkpoint the intermediate groupby product.
-    bok.dask_infra.clean_write_parquet(sites_visited, "scratch/sites_visited")
-    sites_visited = dask.dataframe.read_parquet("scratch/sites_visited", engine="fastparquet")
-
-    # Convert to pandas!
-    # This should be quick since the grouby has already happened.
-    sites_visited = sites_visited.compute()
-    # Get the category column back
-    sites_visited = sites_visited.reset_index()
-    sites_visited = sites_visited.melt(id_vars=["category"], value_vars=["frequency"], var_name="website", value_name="frequency")
-
-    altair.Chart(sites_visited).mark_bar().encode(
-        x="category",
-        y="frequency",
-    ).serve()
-
+    graph_temporary_file = "scratch/graphs/bytes_per_category"
+    reduce_to_pandas(outfile=graph_temporary_file, dask_client=client)
+    chart = make_plot(graph_temporary_file)
