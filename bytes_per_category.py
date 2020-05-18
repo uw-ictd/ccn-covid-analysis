ac54d44ffe04c37d60ee27f3e9f8fa64c683451e
Matt Johnson <matt9j@cs.washington.edu>
2020-05-17 20:36:17 -0700

    Handle unknown categories as other

--- bytes_per_category.py
+++ bytes_per_category.py
@@ -17,6 +17,9 @@ def reduce_to_pandas(outfile, dask_client):
     flows["start_bin"] = flows["start"].dt.floor("d")
     flows = flows.set_index("start_bin")
 
+    # Groupby will drop None category values, so manually reassign to other
+    flows["category"] = flows["category"].fillna("Other")
+
     # Do the grouping
     flows = flows.groupby(["start_bin", "category"]).sum()
     flows = flows.compute()
@@ -29,10 +32,10 @@ def make_plot(infile):
     grouped_flows = grouped_flows.reset_index()
     grouped_flows["bytes_total"] = grouped_flows["bytes_up"] + grouped_flows["bytes_down"]
 
-    test = grouped_flows
-    test = grouped_flows.loc[grouped_flows["category"] == None].groupby("fqdn").sum()
+    # test = grouped_flows
+    # test = grouped_flows.loc[grouped_flows["category"] == None].groupby("fqdn").sum()
 
-    print(test.sort_values("bytes_total"))
+    # print(test.sort_values("bytes_total"))
 
     # Consolidate by week instead of by day
     grouped_flows = grouped_flows[["start_bin", "bytes_total", "category"]].groupby([pd.Grouper(key="start_bin", freq="W-MON"), "category"]).sum()
@@ -40,6 +43,7 @@ def make_plot(infile):
     grouped_flows = grouped_flows.reset_index()
 
     print(grouped_flows)
+    print(grouped_flows["category"].unique())
 
     grouped_flows["GB"] = grouped_flows["bytes_total"] / (1000**3)
     alt.Chart(grouped_flows).mark_area().encode(
@@ -57,7 +61,7 @@ def make_plot(infile):
     ).properties(
         # title="Local Service Use",
         width=500,
-    ).interactive().serve(port=8085, open_browser=False)
+    ).interactive().show()
 
     # .save("renders/bytes_per_category.png",
     #    scale_factor=2
@@ -85,5 +89,5 @@ def make_plot(infile):
 if __name__ == "__main__":
     client = bok.dask_infra.setup_dask_client()
     graph_temporary_file = "scratch/graphs/bytes_per_category"
-    reduce_to_pandas(outfile=graph_temporary_file, dask_client=client)
+    # reduce_to_pandas(outfile=graph_temporary_file, dask_client=client)
     chart = make_plot(graph_temporary_file)
