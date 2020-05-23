88ce3bf99a10eea9a8ebb976111f7e5bdbc5603a
Matt Johnson <matt9j@cs.washington.edu>
2020-05-22 22:17:41 -0700

    Bytes per day of week

--- bytes_per_day_of_week.py
+++ bytes_per_day_of_week.py
@@ -37,7 +37,7 @@ def reduce_to_pandas(outfile, dask_client):
     flows = flows.set_index("start_bin")
 
     # Do the grouping
-    flows = flows.groupby(["start_bin"]).sum()
+    flows = flows.groupby(["start_bin", "day"]).sum()
 
     flows = flows.compute()
 
@@ -47,24 +47,47 @@ def reduce_to_pandas(outfile, dask_client):
 def make_plot(infile):
     grouped_flows = bok.pd_infra.read_parquet(infile)
     grouped_flows = grouped_flows.reset_index()
-    grouped_flows["bytes_total"] = grouped_flows["bytes_up"] + grouped_flows["bytes_down"]
 
-    grouped_flows = grouped_flows.reset_index()
-
-    print(grouped_flows)
     days = ['Monday', 'Tuesday', 'Wednesday',
             'Thursday', 'Friday', 'Saturday', 'Sunday']
 
     grouped_flows["GB"] = grouped_flows["bytes_total"] / (1000**3)
+    working_times = grouped_flows.loc[(grouped_flows["start_bin"] < "2019-07-30") | (grouped_flows["start_bin"] > "2019-08-31")]
+    grouped_flows["outage"] = "Outage"
+    grouped_flows.loc[(grouped_flows["start_bin"] < "2019-07-30") | (grouped_flows["start_bin"] > "2019-08-31"), "outage"] = "Normal"
+    print(grouped_flows)
 
-    alt.Chart(grouped_flows).mark_boxplot().encode(
+    outages = grouped_flows.loc[grouped_flows["GB"] < 0.9]
+    print(outages)
+
+    alt.Chart(working_times).mark_boxplot().encode(
         x=alt.X('day:N',
                 sort=days,
                 title="Day of Week"
                 ),
         y=alt.Y('GB:Q',
-                title="MB Per Day Purchased"
+                title="GB Per Day"
                 ),
+    ).save(
+        "renders/bytes_per_day_of_week_boxplot_exclude_outage.png",
+        scale_factor=2,
+    )
+
+    alt.Chart(grouped_flows).mark_point(opacity=0.3).encode(
+        x=alt.X('day:N',
+                sort=days,
+                title="Day of Week"
+                ),
+        y=alt.Y('GB:Q',
+                title="GB Per Day"
+                ),
+        color=alt.Color(
+            "outage",
+            title="Condition",
+        )
+    ).save(
+        "renders/bytes_per_day_of_week_overplot.png",
+        scale_factor=2,
     )
 
     # plot = alt.Chart(grouped_flows).mark_area().encode(
@@ -90,6 +113,6 @@ def make_plot(infile):
 if __name__ == "__main__":
     client = bok.dask_infra.setup_dask_client()
     graph_temporary_file = "scratch/graphs/bytes_per_day_of_week"
-    create_all_flows(client)
-    reduce_to_pandas(outfile=graph_temporary_file, dask_client=client)
+    # create_all_flows(client)
+    # reduce_to_pandas(outfile=graph_temporary_file, dask_client=client)
     chart = make_plot(graph_temporary_file)
