8db7a1a6e72ec56e738ed6738b804c491661ff20
Matt Johnson <matt9j@cs.washington.edu>
2020-05-23 22:53:34 -0700

    Initial non-normalize heatmap render

--- users_per_category.py
+++ users_per_category.py
@@ -22,37 +22,39 @@ def reduce_to_pandas(outfile, dask_client):
 
 
 def make_category_plot(infile):
+    pd.set_option('display.max_columns', None)
     grouped_flows = bok.pd_infra.read_parquet(infile)
     grouped_flows = grouped_flows.reset_index()
     grouped_flows["bytes_total"] = grouped_flows["bytes_up"] + grouped_flows["bytes_down"]
 
-    print(grouped_flows)
-
     # Figure out sorting order by total amount.
     cat_totals = grouped_flows.groupby("category").sum().reset_index()
-    sort_order = cat_totals.sort_values("bytes_total", ascending=True).set_index("bytes_total").reset_index()
-    sort_list = sort_order["category"].tolist()
-    sort_list.reverse()
-    sort_order["order"] = sort_order.index
+    cat_sort_order = cat_totals.sort_values("bytes_total", ascending=False).set_index("bytes_total").reset_index()
+    cat_sort_list = cat_sort_order["category"].tolist()
 
-    # Merge the sort order back into the larger dataset
-    grouped_flows = grouped_flows.merge(sort_order[["category", "order"]], on="category")
+    user_totals = grouped_flows.groupby("user").sum().reset_index()
+    user_sort_order = user_totals.sort_values("bytes_total", ascending=False).set_index("bytes_total").reset_index()
+    user_sort_list = user_sort_order["user"].tolist()
 
     grouped_flows["GB"] = grouped_flows["bytes_total"] / (1000**3)
+    grouped_flows = grouped_flows[["category", "user", "GB"]].groupby(["user", "category"]).sum()
+    grouped_flows = grouped_flows.reset_index()
+    grouped_flows["logGB"] = grouped_flows["GB"].transform(np.log10)
 
     alt.Chart(grouped_flows).mark_rect().encode(
         x=alt.X("user:N",
-                title="User",
-                axis=alt.Axis(labels=True),
+                title="User (Sorted)",
+                axis=alt.Axis(labels=False),
+                sort=user_sort_list,
                 ),
         y=alt.Y("category:N",
-                title="Category",
-                #stack="normalize"
+                title="Category (Sorted)",
+                sort=cat_sort_list,
                 ),
         # shape="direction",
         color=alt.Color(
-            "GB:Q",
-            title="GB Total",
+            "logGB:Q",
+            title="log(Total GB)",
         ),
     ).properties(
         width=500,
@@ -71,6 +73,6 @@ def make_org_plot(infile):
 if __name__ == "__main__":
     client = bok.dask_infra.setup_dask_client()
     graph_temporary_file = "scratch/graphs/users_per_category"
-    reduce_to_pandas(outfile=graph_temporary_file, dask_client=client)
+    # reduce_to_pandas(outfile=graph_temporary_file, dask_client=client)
     make_category_plot(graph_temporary_file)
     make_org_plot(graph_temporary_file)
