0c9c8dc2ea4035d6d32a7e21e3c5ef4e575942d3
Matt Johnson <matt9j@cs.washington.edu>
2020-05-25 03:16:00 -0700

    Gaps in the log and downtime

--- log_gaps.py
+++ log_gaps.py
@@ -5,11 +5,13 @@ Times where there are no logs in progress at all, across any user,
 could indicate power or enodeb failures.
 """
 
-import altair
-import bok.dask_infra
-import bok.pd_infra
+import altair as alt
 import numpy as np
 import pandas as pd
+
+import bok.constants
+import bok.dask_infra
+import bok.pd_infra
 
 
 def reduce_flow_gaps_to_pandas(outfile, dask_client):
@@ -76,13 +78,75 @@ def reduce_flow_gaps_to_pandas(outfile, dask_client):
     bok.pd_infra.clean_write_parquet(gaps_frame, outfile)
 
 
-def make_plot(infile):
-    raise NotImplementedError("No plot 4 u")
+def make_timeline_plot(infile):
+    gaps = bok.pd_infra.read_parquet(infile)
+    print(gaps.head())
+    alt.Chart(gaps).mark_rect().encode(
+        x=alt.X("start:T"),
+        x2=alt.X2("end:T"),
+    ).properties(
+        width=1000,
+    ).save(
+        "renders/log_gaps_downtime.png",
+        scale_factor=2,
+    )
+
+
+def compute_uptime_stats(infile):
+    gaps = bok.pd_infra.read_parquet(infile)
+
+    gaps["duration"] = gaps["end"] - gaps["start"]
+    downtime = gaps["duration"].sum()
+    total_time = bok.constants.MAX_DATE - bok.constants.MIN_DATE
+
+    print("Downtime", downtime)
+    print("Total time", total_time)
+    print("Percentage", downtime/total_time * 100)
+
+
+def make_outage_duration_cdf_plot(infile):
+    gaps = bok.pd_infra.read_parquet(infile)
+
+    gaps["duration"] = gaps["end"] - gaps["start"]
+
+    # Only record as an outage if the duration is longer than 10s. This is
+    # shorter than the reasonable recovery time of the epc. Shorter gaps are
+    # possible in the normal flow of traffic, especially at lightly loaded
+    # hours when there may only be a few flows and low overlap.
+    gaps = gaps.loc[gaps["duration"] >= pd.Timedelta(seconds=10)]
+
+    # Convert to Hours
+    gaps["duration_hours"] = gaps["duration"].dt.total_seconds() / 3600
+
+    stats_frame = compute_cdf(gaps, "duration_hours", "start")
+
+    alt.Chart(stats_frame).mark_line(interpolate='step-after', clip=True).encode(
+        x=alt.X('duration_hours:Q',
+                scale=alt.Scale(type="log"),
+                title="Duration of Outage (Hours)"
+                ),
+        y=alt.Y('cdf',
+                title="Fraction of Recorded Outages (CDF)",
+                scale=alt.Scale(type="linear", domain=(0, 1.0)),
+                ),
+    ).properties(
+        width=500,
+    ).save("renders/log_gaps_outage_duration_cdf.png", scale_factor=2.0)
+
+
+def compute_cdf(frame, value_column, base_column):
+    # Find the PDF first
+    stats_frame = frame.groupby(value_column).count()[[base_column]].rename(columns = {base_column: "base_count"})
+    stats_frame["pdf"] = stats_frame["base_count"] / sum(stats_frame["base_count"])
+    stats_frame["cdf"] = stats_frame["pdf"].cumsum()
+    stats_frame = stats_frame.reset_index()
+    return stats_frame
 
 
 if __name__ == "__main__":
     client = bok.dask_infra.setup_dask_client()
     graph_temporary_file = "scratch/graphs/flow_gaps"
-    reduce_flow_gaps_to_pandas(outfile=graph_temporary_file, dask_client=client)
-    chart = make_plot(graph_temporary_file)
-    chart.interactive().serve(port=8891, open_browser=False)
+    # reduce_flow_gaps_to_pandas(outfile=graph_temporary_file, dask_client=client)
+    make_timeline_plot(graph_temporary_file)
+    make_outage_duration_cdf_plot(graph_temporary_file)
+    compute_uptime_stats(graph_temporary_file)
