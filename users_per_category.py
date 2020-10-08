dd85024c39b811990e4f77cc0df1047cd37ee449
Matt Johnson <matt9j@cs.washington.edu>
2020-10-07 17:38:27 -0700

    Cleaning up users_per_category

--- users_per_category.py
+++ users_per_category.py
@@ -302,7 +302,7 @@ def make_org_plot(infile):
     )
 
 
-def make_category_quantiles_plot(infile):
+def make_category_quantiles_plots(infile):
     grouped_flows = bok.pd_infra.read_parquet(infile)
     grouped_flows = grouped_flows.reset_index()
     grouped_flows["bytes_total"] = grouped_flows["bytes_up"] + grouped_flows["bytes_down"]
@@ -383,11 +383,10 @@ def make_category_quantiles_plot(infile):
     # users some users that dominate video could be overrepresented. Maybe
     # want to merge on the fraction of traffic to each part from each user?
     # Are users counted equally or are bytes counted equally...
-    alt.Chart(quantile_totals[["category", "quantile_str", "bytes_total", "rank", "normalized_bytes_total"]]).mark_point().encode(
+    alt.Chart(quantile_totals[["category", "quantile_str", "bytes_total", "rank", "normalized_bytes_total"]]).mark_line().encode(
         x="quantile_str:N",
         y=alt.Y(
             "normalized_bytes_total",
-            #stack="normalize",
             sort=cat_sort_list,
             title="average bytes per online day"
         ),
@@ -408,6 +407,137 @@ def make_category_quantiles_plot(infile):
     )
 
 
+def make_category_per_user_plots(infile):
+    grouped_flows = bok.pd_infra.read_parquet(infile)
+    grouped_flows = grouped_flows.reset_index()
+    grouped_flows["bytes_total"] = grouped_flows["bytes_up"] + grouped_flows["bytes_down"]
+    user_category_total = grouped_flows[["user", "category", "bytes_total"]].groupby(
+        ["user", "category"]
+    ).sum().reset_index()
+
+    # Filter users by time in network to eliminate early incomplete samples
+    user_active_ranges = bok.pd_infra.read_parquet(
+        "data/clean/user_active_deltas.parquet")[["user", "days_since_first_active", "days_active", "days_online"]]
+    # Drop users that joined less than a week ago.
+    users_to_analyze = user_active_ranges.loc[
+        user_active_ranges["days_since_first_active"] > 7
+        ]
+    # Drop users active for less than one day
+    users_to_analyze = users_to_analyze.loc[
+        users_to_analyze["days_active"] > 1,
+    ]
+
+    # Sort categories by total amount of bytes.
+    cat_totals = grouped_flows.groupby("category").sum().reset_index()
+    cat_sort_order = cat_totals.sort_values("bytes_total", ascending=False).set_index("bytes_total").reset_index()
+    cat_sort_order["cat_rank"] = cat_sort_order["bytes_total"].rank(method="min", ascending=False)
+    cat_sort_list = cat_sort_order["category"].tolist()
+
+    # Rank users by their daily use.
+    user_totals = user_category_total.groupby("user").sum().reset_index()
+    user_totals = user_totals.merge(users_to_analyze, on="user", how="inner")
+    user_totals["user_total_bytes_avg_online_day"] = user_totals["bytes_total"] / user_totals["days_online"]
+    user_totals["user_rank"] = user_totals["user_total_bytes_avg_online_day"].rank(method="min")
+
+    # take the minimum of days online and days active, since active is
+    # partial-day aware, but online rounds up to whole days. Can be up to 2-e
+    # days off if the user joined late in the day and was last active early.
+    user_totals["normalized_days_online"] = np.minimum(
+        user_totals["days_online"], user_totals["days_active"]) / user_totals["days_active"]
+
+    user_category_total = user_category_total.merge(
+        user_totals[["user", "user_rank", "days_online", "user_total_bytes_avg_online_day"]],
+        on="user",
+        how="inner"
+    )
+    user_category_total = user_category_total.merge(cat_sort_order[["category", "cat_rank"]], on="category", how="inner")
+    print(user_category_total)
+
+    user_category_total["bytes_avg_online_day"] = user_category_total["bytes_total"] / user_category_total["days_online"]
+    user_category_total["share_of_bytes_avg_online_day"] = \
+        user_category_total["bytes_avg_online_day"] / user_category_total["user_total_bytes_avg_online_day"]
+    print(user_category_total)
+
+    # This might not be showing exactly what I want to show, since in merging
+    # users some users that dominate video could be overrepresented. Maybe
+    # want to merge on the fraction of traffic to each part from each user?
+    # Are users counted equally or are bytes counted equally...
+    alt.Chart(user_category_total[["category", "user_rank", "cat_rank", "bytes_avg_online_day"]]).mark_bar().encode(
+        x="user_rank:O",
+        y=alt.Y(
+            "bytes_avg_online_day",
+            stack="normalize",
+            sort=cat_sort_list,
+        ),
+        color=alt.Color(
+            "category:N",
+            scale=alt.Scale(scheme="tableau20"),
+            sort=cat_sort_list,
+        ),
+        order=alt.Order(
+            "cat_rank",
+            sort="descending",
+        ),
+    ).properties(
+        width=500,
+    ).save(
+        "renders/bytes_per_average_online_day_per_user_bar.png",
+        scale_factor=2,
+    )
+
+    alt.Chart(user_category_total[["category", "user_rank", "cat_rank", "bytes_avg_online_day"]]).mark_point(
+        size=10,
+        strokeWidth=2,
+    ).encode(
+        x="user_rank:O",
+        y=alt.Y(
+            "bytes_avg_online_day",
+            sort=cat_sort_list,
+            title="average bytes per online day"
+        ),
+        color=alt.Color(
+            "category:N",
+            scale=alt.Scale(scheme="tableau20"),
+            sort=cat_sort_list,
+        ),
+        order=alt.Order(
+            "cat_rank",
+            sort="descending",
+        ),
+    ).properties(
+        width=500,
+    ).save(
+        "renders/bytes_per_average_online_day_per_user_points.png",
+        scale_factor=2,
+    )
+
+    alt.Chart(user_category_total[["category", "user_rank", "cat_rank", "share_of_bytes_avg_online_day"]]).mark_point(
+        size=10,
+        strokeWidth=2,
+    ).encode(
+        x="user_rank:O",
+        y=alt.Y(
+            "share_of_bytes_avg_online_day",
+            sort=cat_sort_list,
+            title="share of average bytes per online day"
+        ),
+        color=alt.Color(
+            "category:N",
+            scale=alt.Scale(scheme="tableau20"),
+            sort=cat_sort_list,
+        ),
+        order=alt.Order(
+            "cat_rank",
+            sort="descending",
+        ),
+    ).properties(
+        width=500,
+    ).save(
+        "renders/share_of_bytes_per_average_online_day_per_user_points.png",
+        scale_factor=2,
+    )
+
+
 if __name__ == "__main__":
     platform = bok.platform.read_config()
     graph_temporary_file = "scratch/graphs/users_per_category"
@@ -421,7 +551,8 @@ if __name__ == "__main__":
     if platform.altair_support:
         print("Running vis tasks")
         pd.set_option('display.max_columns', None)
-        make_category_quantiles_plot(graph_temporary_file)
+        make_category_quantiles_plots(graph_temporary_file)
+        make_category_per_user_plots(graph_temporary_file)
 
         # make_category_plot(graph_temporary_file)
         # make_org_plot(graph_temporary_file)
