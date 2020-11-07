1e608de9aca0d664e07b50140e0e162249696100
Matt Johnson <matt9j@cs.washington.edu>
2020-11-06 17:09:08 -0800

    Remove unused code from bytes per week

--- bytes_per_week.py
+++ bytes_per_week.py
@@ -15,45 +15,6 @@ import infra.dask
 import infra.pd
 import infra.platform
 
-# Configs
-day_intervals = 7
-# IMPORTANT: Run get_date_range() to update these values when loading in a new dataset!
-max_date = infra.constants.MAX_DATE
-
-def cohort_as_date_interval(x):
-    cohort_start = max_date - datetime.timedelta(day_intervals * x + day_intervals - 1)
-    cohort_end = max_date - datetime.timedelta(day_intervals * x)
-
-    return cohort_start.strftime("%Y/%m/%d") + "-" + cohort_end.strftime("%Y/%m/%d")
-
-def cohort_as_date(x):
-    day = max_date - datetime.timedelta(day_intervals * x)
-    return day.strftime("%Y/%m/%d")
-
-def get_cohort(x):
-    return x["start"].apply(lambda x_1: (max_date - x_1).days // day_intervals, meta=('start', 'int64'))
-
-def get_date(x):
-    return x["cohort"].apply(cohort_as_date_interval, meta=('cohort', 'object'))
-
-def get_throughput_data(flows):
-    # Make indexes a column and select "start", "bytes_up", "bytes_down" columns
-    query = flows.reset_index()[["start", "bytes_up", "bytes_down"]]
-    # Map each start to a cohort
-    query = query.assign(cohort=get_cohort)
-    # Group by cohorts and get the all the users
-    query = query.groupby("cohort")
-    # Sum up all of the bytes_up and bytes_down
-    query = query.sum()
-    # Get the start column back
-    query = query.reset_index()
-    # Get the total usage per day
-    query["total_bytes"] = query["bytes_up"] + query["bytes_down"]
-    # Get each date mapped to each cohort
-    query = query.assign(date=get_date)
-
-    return query
-
 
 def reduce_to_pandas(outfile, dask_client):
     typical = infra.dask.read_parquet("data/clean/flows/typical_fqdn_org_category_local_TM_DIV_none_INDEX_start")[["bytes_up", "bytes_down"]]
