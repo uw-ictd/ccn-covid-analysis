fbc376e0474b486f70673538756b9f3d79b270e9
Matt Johnson <matt9j@cs.washington.edu>
2020-12-27 15:50:50 -0800

    preprocessing: Remove old unused code from shift to local time

--- preprocessing/shift_to_local_time.py
+++ preprocessing/shift_to_local_time.py
@@ -1,6 +1,4 @@
 import pandas as pd
-import dask.dataframe
-import os
 
 import infra.constants
 import infra.dask
@@ -10,96 +8,24 @@ import infra.platform
 
 def shift_flows(in_path, out_path):
     df = infra.dask.read_parquet(in_path)
-
     df = df.reset_index()
-    df["start"] = df["start"] + pd.tseries.offsets.DateOffset(
+
+    df["start"] = df["start"] + pd.Timedelta(
         hours=infra.constants.LOCAL_TIME_UTC_OFFSET_HOURS)
-    df["end"] = df["end"] + pd.tseries.offsets.DateOffset(
+    df["end"] = df["end"] + pd.Timedelta(
         hours=infra.constants.LOCAL_TIME_UTC_OFFSET_HOURS)
+
     df = df.set_index("start")
 
     return infra.dask.clean_write_parquet(df, out_path, compute=False)
 
 
-def shift_dns(in_path, out_path):
-    df = infra.dask.read_parquet(in_path)
-    df = df.reset_index()
-    df["timestamp"] = df["timestamp"] + pd.tseries.offsets.DateOffset(
-        hours=infra.constants.LOCAL_TIME_UTC_OFFSET_HOURS)
-    df = df.set_index("timestamp")
-
-    return infra.dask.clean_write_parquet(df, out_path, compute=False)
-
-
-def map_directory_recursive(in_path, out_path, map_fn, client):
-    subfiles = sorted(os.listdir(in_path))
-
-    future_handles = []
-    for subfile in subfiles:
-        df_path = os.path.join(in_path, subfile)
-        df_out_path = os.path.join(out_path, subfile)
-
-        future_handles.append(map_fn(df_path, df_out_path))
-
-    print("Recursive flow shift now")
-    client.compute(future_handles, sync=True)
-
-
-def shift_flows_flat_noindex(in_path, out_path):
-    df = dask.dataframe.read_parquet(in_path, engine="fastparquet")
-    df["start"] = df["start"] + pd.tseries.offsets.DateOffset(hours=9)
-    df["end"] = df["end"] + pd.tseries.offsets.DateOffset(hours=9)
-    print("Single layer flow shift now")
-    infra.dask.clean_write_parquet(df, out_path)
-
-
-def shift_dns_flat(in_path, out_path):
-    df = dask.dataframe.read_parquet(in_path, engine="fastparquet")
-    df = df.reset_index()
-    df["timestamp"] = df["timestamp"] + pd.tseries.offsets.DateOffset(hours=9)
-    print("Single layer dns shift now")
-    df = df.set_index("timestamp")
-    infra.dask.clean_write_parquet(df, out_path)
-
-
 def shift_transactions_flat_noindex(in_path, out_path):
     df = infra.pd.read_parquet(in_path)
-    df["timestamp"] = df["timestamp"] + pd.tseries.offsets.DateOffset(hours=9)
+    df["timestamp"] = df["timestamp"] + pd.Timedelta(
+        hours=infra.constants.LOCAL_TIME_UTC_OFFSET_HOURS)
     print("Single layer timestamp shift now")
     infra.pd.clean_write_parquet(df, out_path)
-
-
-def _shift_existing_data(client):
-    """A legacy function used to apply the shift to all existing data. Not
-    part of the normal pipeline. """
-    map_directory_recursive(
-        "data/clean/flows/typical_fqdn_DIV_user_INDEX_start",
-        "data/clean/flows/typical_fqdn_TZ_DIV_user_INDEX_start",
-        shift_flows,
-        client,
-    )
-
-    map_directory_recursive(
-        "data/clean/dns/successful_DIV_user_INDEX_timestamp",
-        "data/clean/dns/successful_TZ_DIV_user_INDEX_timestamp",
-        shift_dns,
-        client,
-    )
-
-    shift_flows_flat_noindex("data/clean/flows/typical_DIV_none_INDEX_user",
-                             "data/clean/flows/typical_TZ_DIV_none_INDEX_user")
-
-    shift_flows_flat_noindex("data/clean/flows/nouser_DIV_none_INDEX_none",
-                             "data/clean/flows/nouser_TZ_DIV_none_INDEX_none")
-
-    shift_flows_flat_noindex("data/clean/flows/p2p_DIV_none_INDEX_none",
-                             "data/clean/flows/p2p_TZ_DIV_none_INDEX_none")
-
-    shift_dns_flat("data/clean/dns/successful_DIV_none_INDEX_timestamp",
-                   "data/clean/dns/successful_TZ_DIV_none_INDEX_timestamp")
-
-    shift_transactions_flat_noindex("data/clean/transactions",
-                                    "data/clean/transactions_TZ")
 
 
 def shift_all(client):
@@ -123,8 +49,9 @@ def shift_all(client):
     shift_transactions_flat_noindex("scratch/transactions.parquet",
                                     "scratch/transactions_TZ.parquet")
 
+
 if __name__ == "__main__":
     platform = infra.platform.read_config()
-    dask_client = infra.dask.setup_platform_tuned_dask_client(10, platform)
+    dask_client = infra.dask.setup_platform_tuned_dask_client(20, platform)
 
     shift_all(client=dask_client)
