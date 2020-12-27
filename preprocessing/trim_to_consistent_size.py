5b1333e8d0b20517e4f447fc4b6445b8c4bddfaa
Matt Johnson <matt9j@cs.washington.edu>
2020-12-27 12:53:36 -0800

    preprocessing: Refactor trim for new pipeline ordering

--- preprocessing/trim_to_consistent_size.py
+++ preprocessing/trim_to_consistent_size.py
@@ -1,124 +1,55 @@
-import pandas as pd
-import dask.dataframe
-import os
-
+import infra.constants
 import infra.dask
-
-def trim_flows_recursive(in_path, out_path, client):
-    subfiles = sorted(os.listdir(in_path))
-
-    future_handles = []
-    for subfile in subfiles:
-        df_path = os.path.join(in_path, subfile)
-        df_out_path = os.path.join(out_path, subfile)
-
-        df = dask.dataframe.read_parquet(df_path, engine="fastparquet")
-        df = df.loc[(df.index >= '2019-03-10 00:00') & (df.index < '2020-05-03 00:00')]
-
-        df = df.astype({"user_port": int,
-                        "dest_port": int,
-                        "bytes_up": int,
-                        "bytes_down": int,
-                        "protocol": int,
-                        "ambiguous_fqdn_count": int
-                        })
-
-        handle = infra.dask.clean_write_parquet(df, df_out_path, compute=False)
-        future_handles.append(handle)
-
-    print("Recursive flow trim now")
-    client.compute(future_handles, sync=True)
+import infra.pd
+import infra.platform
 
 
-def trim_dns_recursive(in_path, out_path, client):
-    subfiles = sorted(os.listdir(in_path))
-
-    future_handles = []
-    for subfile in subfiles:
-        df_path = os.path.join(in_path, subfile)
-        df_out_path = os.path.join(out_path, subfile)
-
-        df = dask.dataframe.read_parquet(df_path, engine="fastparquet")
-        df = df.loc[(df.index >= '2019-03-10 00:00') & (df.index < '2020-05-03 00:00')]
-
-        handle = infra.dask.clean_write_parquet(df, df_out_path, compute=False)
-        future_handles.append(handle)
-
-    print("Recursive dns trim now")
-    client.compute(future_handles, sync=True)
+MIN_DATE = infra.constants.MIN_DATE
+MAX_DATE = infra.constants.MAX_DATE
 
 
-def trim_flows_flat_noindex(in_path, out_path):
-    df = dask.dataframe.read_parquet(in_path, engine="fastparquet")
-    df = df.loc[(df["start"] >= '2019-03-10 00:00') & (df["start"] < '2020-05-03 00:00')]
-    df = df.astype({"user_port": int,
-                    "dest_port": int,
-                    "bytes_up": int,
-                    "bytes_down": int,
-                    "protocol": int,
-                    })
+def trim_flows(in_path, out_path):
+    df = infra.dask.read_parquet(in_path)
+    df = df.loc[(df.index >= MIN_DATE) & (df.index < MAX_DATE)]
 
-    print("Single layer flow trim now")
-    infra.dask.clean_write_parquet(df, out_path)
-
-
-def trim_atypical_flows_flat_noindex(in_path, out_path):
-    df = dask.dataframe.read_parquet(in_path, engine="fastparquet")
-    df = df.loc[(df["start"] >= '2019-03-10 00:00') & (df["start"] < '2020-05-03 00:00')]
-    df = df.astype({"a_port": int,
-                    "b_port": int,
-                    "bytes_a_to_b": int,
-                    "bytes_b_to_a": int,
-                    "protocol": int,
-                    })
-
-    print("Single layer flow trim now")
-    infra.dask.clean_write_parquet(df, out_path)
-
-
-def trim_dns_flat(in_path, out_path):
-    df = dask.dataframe.read_parquet(in_path, engine="fastparquet")
-    df = df.loc[(df.index >= '2019-03-10 00:00') & (df.index < '2020-05-03 00:00')]
-
-    print("Single layer dns trim now")
-    infra.dask.clean_write_parquet(df, out_path)
+    return infra.dask.clean_write_parquet(df, out_path, compute=False)
 
 
 def trim_transactions_flat_noindex(in_path, out_path):
-    df = dask.dataframe.read_parquet(in_path, engine="fastparquet")
-    df = df.loc[(df["timestamp"] >= '2019-03-10 00:00') & (df["timestamp"] < '2020-05-03 00:00')]
+    df = infra.pd.read_parquet(in_path)
+    df = df.loc[(df["timestamp"] >= MIN_DATE) & (df["timestamp"] < MAX_DATE)]
     print("Single layer transaction trim  now")
     df["amount_bytes"] = df["amount_bytes"].fillna(value=0)
     df = df.astype({"amount_bytes": int, "amount_idr": int})
-    infra.dask.clean_write_parquet(df, out_path)
+    infra.pd.clean_write_parquet(df, out_path)
+
+
+def trim_all(client):
+    future_handles = [
+        trim_flows(
+            "scratch/flows/typical_fqdn_org_category_local_TZ_DIV_none_INDEX_start",
+            "scratch/flows/typical_fqdn_org_category_local_TM_DIV_none_INDEX_start",
+        ),
+        trim_flows(
+            "scratch/flows/p2p_TZ_DIV_none_INDEX_start",
+            "scratch/flows/p2p_TM_DIV_none_INDEX_start",
+        ),
+        trim_flows(
+            "scratch/flows/nouser_TZ_DIV_none_INDEX_start",
+            "scratch/flows/nouser_TM_DIV_none_INDEX_start",
+        )
+    ]
+    client.compute(future_handles, sync=True)
+
+    trim_transactions_flat_noindex(
+        "scratch/transactions_TZ.parquet",
+        "scratch/transactions_TM.parquet",
+    )
 
 
 if __name__ == "__main__":
-    client = infra.dask.setup_dask_client()
-    trim_flows_recursive("data/clean/flows/typical_fqdn_TZ_DIV_user_INDEX_start",
-                         "data/clean/flows/typical_fqdn_TM_DIV_user_INDEX_start",
-                         client)
+    platform = infra.platform.read_config()
+    dask_client = infra.dask.setup_platform_tuned_dask_client(10, platform)
 
-    trim_flows_flat_noindex("data/clean/flows/typical_TZ_DIV_none_INDEX_user",
-                            "data/clean/flows/typical_TM_DIV_none_INDEX_user")
-
-    trim_atypical_flows_flat_noindex(
-        "data/clean/flows/nouser_TZ_DIV_none_INDEX_none",
-        "data/clean/flows/nouser_TM_DIV_none_INDEX_none"
-    )
-
-    trim_atypical_flows_flat_noindex(
-        "data/clean/flows/p2p_TZ_DIV_none_INDEX_none",
-        "data/clean/flows/p2p_TM_DIV_none_INDEX_none"
-    )
-
-    trim_dns_recursive("data/clean/dns/successful_TZ_DIV_user_INDEX_timestamp",
-                       "data/clean/dns/successful_TM_DIV_user_INDEX_timestamp",
-                       client)
-
-    trim_dns_flat("data/clean/dns/successful_TZ_DIV_none_INDEX_timestamp",
-                  "data/clean/dns/successful_TM_DIV_none_INDEX_timestamp")
-
-    trim_transactions_flat_noindex("data/clean/transactions_TZ",
-                                   "data/clean/transactions_TM")
+    trim_all(client=dask_client)
 
