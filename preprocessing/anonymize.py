4bff9f5430d31dbb7df3f9f1ddbdab32df3a4a95
Matt Johnson <matt9j@cs.washington.edu>
2020-12-27 15:04:54 -0800

    preprocessing: Fix anonymization merge and add status printing

--- preprocessing/anonymize.py
+++ preprocessing/anonymize.py
@@ -7,31 +7,30 @@ import infra.platform
 
 
 def anonymize_rare_orgs(in_path, out_path):
+    print("Anonymizing orgs")
     flows = infra.dask.read_parquet(in_path)
     user_counts_per_org = flows[["user", "org", "bytes_up"]].groupby(["user", "org"]).first().dropna().reset_index()
     user_counts_per_org = user_counts_per_org.assign(user_count=1).groupby(["org"]).sum()
-    user_counts_per_org = user_counts_per_org.drop("bytes_up", axis=1)
+    user_counts_per_org = user_counts_per_org.drop("bytes_up", axis=1).reset_index()
     flows_with_counts = flows.merge(user_counts_per_org, on="org")
-    flows_with_counts = flows_with_counts.astype({
-        "org": object,
-    })
+
     flows_with_counts["org"] = flows_with_counts["org"].mask(
         flows_with_counts["user_count"] < infra.constants.MIN_K_ANON,
         other="[Other Anonymized U<{}]".format(infra.constants.MIN_K_ANON),
         )
+
     flows = flows_with_counts.drop("user_count", axis=1)
     infra.dask.clean_write_parquet(flows, out_path)
 
 
 def anonymize_rare_fqdns(in_path, out_path):
+    print("Anonymizing fqdns")
     flows = infra.dask.read_parquet(in_path)
     user_counts_per_org = flows[["user", "fqdn", "bytes_up"]].groupby(["user", "fqdn"]).first().dropna().reset_index()
     user_counts_per_org = user_counts_per_org.assign(user_count=1).groupby(["fqdn"]).sum()
-    user_counts_per_org = user_counts_per_org.drop("bytes_up", axis=1)
+    user_counts_per_org = user_counts_per_org.drop("bytes_up", axis=1).reset_index()
     flows_with_counts = flows.merge(user_counts_per_org, on="fqdn")
-    flows_with_counts = flows_with_counts.astype({
-        "fqdn": object,
-    })
+
     flows_with_counts["fqdn"] = flows_with_counts["fqdn"].mask(
         flows_with_counts["user_count"] < infra.constants.MIN_K_ANON,
         other="[Other Anonymized U<{}]".format(infra.constants.MIN_K_ANON),
@@ -41,14 +40,13 @@ def anonymize_rare_fqdns(in_path, out_path):
 
 
 def anonymize_rare_ips(in_path, out_path):
+    print("Anonymizing destination ip addresses")
     flows = infra.dask.read_parquet(in_path)
     user_counts_per_org = flows[["user", "dest_ip", "bytes_up"]].groupby(["user", "dest_ip"]).first().dropna().reset_index()
     user_counts_per_org = user_counts_per_org.assign(user_count=1).groupby(["dest_ip"]).sum()
-    user_counts_per_org = user_counts_per_org.drop("bytes_up", axis=1)
+    user_counts_per_org = user_counts_per_org.drop("bytes_up", axis=1).reset_index()
     flows_with_counts = flows.merge(user_counts_per_org, on="dest_ip")
-    flows_with_counts = flows_with_counts.astype({
-        "dest_ip": object,
-    })
+
     flows_with_counts["dest_ip"] = flows_with_counts["dest_ip"].mask(
         flows_with_counts["user_count"] < infra.constants.MIN_K_ANON,
         other="[Other Anonymized U<{}]".format(infra.constants.MIN_K_ANON),
