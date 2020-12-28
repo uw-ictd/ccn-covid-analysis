3720c8dd12240b98e16234a6b3d8c200c8067a06
Matt Johnson <matt9j@cs.washington.edu>
2020-12-27 16:58:57 -0800

    preprocessing: Fix anonymization indexing

--- preprocessing/anonymize.py
+++ preprocessing/anonymize.py
@@ -9,6 +9,12 @@ import infra.platform
 def anonymize_rare_orgs(in_path, out_path):
     print("Anonymizing orgs")
     flows = infra.dask.read_parquet(in_path)
+
+    # Reset the index immediately to preserve the start column across
+    # groupings. Don't reindex until after all the anonymization aggregates
+    # and groups are complete to avoid unnecessary computation.
+    flows = flows.reset_index()
+
     user_counts_per_org = flows[["user", "org", "bytes_up"]].groupby(["user", "org"]).first().dropna().reset_index()
     user_counts_per_org = user_counts_per_org.assign(user_count=1).groupby(["org"]).sum()
     user_counts_per_org = user_counts_per_org.drop("bytes_up", axis=1).reset_index()
@@ -17,7 +23,7 @@ def anonymize_rare_orgs(in_path, out_path):
     flows_with_counts["org"] = flows_with_counts["org"].mask(
         flows_with_counts["user_count"] < infra.constants.MIN_K_ANON,
         other="[Other Anonymized U<{}]".format(infra.constants.MIN_K_ANON),
-        )
+    )
 
     flows = flows_with_counts.drop("user_count", axis=1)
     infra.dask.clean_write_parquet(flows, out_path)
@@ -52,21 +58,22 @@ def anonymize_rare_ips(in_path, out_path):
         other="[Other Anonymized U<{}]".format(infra.constants.MIN_K_ANON),
         )
     flows = flows_with_counts.drop("user_count", axis=1)
+    print(flows)
     infra.dask.clean_write_parquet(flows, out_path)
 
 
 def anonymize_all(client):
     anonymize_rare_orgs(
         "scratch/flows/typical_fqdn_org_category_local_TM_DIV_none_INDEX_start",
-        "scratch/flows/typical_fqdn_org_category_local_TM_DIV_none_INDEX_start_ANON_org",
+        "scratch/flows/typical_fqdn_org_category_local_TM_DIV_none_INDEX_none_ANON_org",
     )
     anonymize_rare_fqdns(
-        "scratch/flows/typical_fqdn_org_category_local_TM_DIV_none_INDEX_start_ANON_org",
-        "scratch/flows/typical_fqdn_org_category_local_TM_DIV_none_INDEX_start_ANON_org_fqdn",
+        "scratch/flows/typical_fqdn_org_category_local_TM_DIV_none_INDEX_none_ANON_org",
+        "scratch/flows/typical_fqdn_org_category_local_TM_DIV_none_INDEX_none_ANON_org_fqdn",
     )
     anonymize_rare_ips(
-        "scratch/flows/typical_fqdn_org_category_local_TM_DIV_none_INDEX_start_ANON_org_fqdn",
-        "scratch/flows/typical_fqdn_org_category_local_TM_DIV_none_INDEX_start_ANON_org_fqdn_ip",
+        "scratch/flows/typical_fqdn_org_category_local_TM_DIV_none_INDEX_none_ANON_org_fqdn",
+        "scratch/flows/typical_fqdn_org_category_local_TM_DIV_none_INDEX_none_ANON_org_fqdn_ip",
     )
 
 
