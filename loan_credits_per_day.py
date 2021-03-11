cd0ee2d560ed3e58e82128688d90e57ee31ab8da
nussarafirn <baifirn@uw.edu>
2021-03-10 22:22:35 -0800

    working on normalize data for same timeline bf/after

--- loan_credits_per_day.py
+++ loan_credits_per_day.py
@@ -20,24 +20,58 @@ def get_data(timeline):
   
   # excluded retailers
   retailers = df_admin["dest_user"].unique()
-  data_cleaned = transactions[~transactions['user'].isin(retailers)]
+  excluded_retailers = transactions[~transactions['user'].isin(retailers)]
 
-  # TODO only get users present bf - after
+  # normalize by time: taking temperal average divide by months
+  # temperal average -- 238 days after (11/24/2020) and before (8/7/2019)
+  start_date = pd.to_datetime("2019-08-07  00:00:00")
+  lockdown_date = pd.to_datetime("2020-04-01  00:00:00")
 
-  # TODO normalize by time 1) 7month before/after  2) taking temperal average divide by months
+  num_days = (lockdown_date - start_date).days
+  query_start = "(timestamp >= '2019-08-07  00:00:00')"
+  
+  # Normalization: only get users present bf - after
+  ## get only users from 238 days before
+  query_before = "(timestamp >= '2019-08-07  00:00:00') & (timestamp < '2020-04-01 00:00:00')"
+  df_range = excluded_retailers.query(query_start)
 
+  users_before = []
+  users_before = df_range.query(query_before)['user'].unique()
+  users_before = users_before[users_before != None]
+
+  dest_users = df_range.query(query_before)['dest_user'].unique()
+  users_before = np.append(users_before, dest_users[dest_users != None])
+  users_before = users_before.unique()
+
+  data_cleaned = []
+  
   # split the timeline before and after COVID
   ## March 25th, 2020 school closes  April 1st, 2020 roads to capital closed to town 
   if timeline == 'before':
-    data_cleaned =  data_cleaned.query("timestamp < '2020-04-01 00:00:00' ")
+    # 238 day before (8/7/2019)
+    data_cleaned =  df_range.query(query_before)
+    print("num users before" + str(data_cleaned['user'].nunique())) 
+
   elif timeline == 'after':
-    data_cleaned =  data_cleaned.query("timestamp >= '2020-04-01 00:00:00' ")
+    df_after =  df_range.query("timestamp >= '2020-04-01 00:00:00' ")
+
+     #  the dest users be from the same datatset too
+    df = df_after[df_after['user'].isin(users_before)]                              
+    df = df_after[df_after['dest_user'].isin(users_before)]  
+
+    df = df.merge(users_before, how = 'outer', on=['user', 'dest_user'])
+    data_cleaned = df['amount_idr'].fillna(0)
+
+    print("num users after" + str(data_cleaned['user'].nunique())) 
+
   else:
     print("Timeline should be only 'before' or 'after' the pandemic lockdown.")
-    return NULL
+    # TODO raise error
+  
 
   # total loan for each user
-  total_loan = data_cleaned.groupby(["user", "dest_user"]).agg({"amount_idr" : 'sum'})
+  total_loan = data_cleaned.groupby(["user", "dest_user"]).agg({"amount_idr" : 'sum'}) 
+  total_loan = total_loan['amount_idr'].div(num_days)
   
   # optional: exclude the maximum amount of loan (entire row)
   total_loan = total_loan[total_loan['amount_idr'] != total_loan['amount_idr'].max()]
@@ -81,5 +115,9 @@ if __name__ == "__main__":
     pd.set_option('display.width', None)
     pd.set_option('display.max_rows', None)
     
-    make_plot(get_data("before"), "before")
-    make_plot(get_data("after"), "after")
+    get_data("before")
+    
+    # get_data("after")
+
+    # make_plot(get_data("before"), "before")
+    # make_plot(get_data("after"), "after")
