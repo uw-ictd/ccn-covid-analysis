fix the normalization and log scale graphs

loan_credits_per_day.py
@@ -1,5 +1,6 @@
 """
 Exploring the loan credits per users.
+Version 3/24: It seems hard to find a meeting in the graphs without any sorting order or animations.
 """
 
 import altair as alt
@@ -23,58 +24,47 @@ def get_data(timeline):
   excluded_retailers = transactions[~transactions['user'].isin(retailers)]
 
   # normalize by time: taking temperal average divide by months
-  # temperal average -- 238 days after (11/24/2020) and before (8/7/2019)
-  start_date = pd.to_datetime("2019-08-07  00:00:00")
-  lockdown_date = pd.to_datetime("2020-04-01  00:00:00")
+  # temperal average -- 238 days after (@11/24/2020) and before (@8/7/2019)
+  start_date = "2019-08-07  00:00:00"
+  lockdown_date = "2020-04-01  00:00:00"
+  num_days = (pd.to_datetime(lockdown_date) - pd.to_datetime(start_date)).days
+  end_date = str(pd.to_datetime(lockdown_date) + pd.DateOffset(num_days))
+  assert(num_days == (pd.to_datetime(end_date) - pd.to_datetime(lockdown_date)).days)
 
-  num_days = (lockdown_date - start_date).days
-  query_start = "(timestamp >= '2019-08-07  00:00:00')"
-  
   # Normalization: only get users present bf - after
   ## get only users from 238 days before
-  query_before = "(timestamp >= '2019-08-07  00:00:00') & (timestamp < '2020-04-01 00:00:00')"
+  query_start = f"(timestamp >= '{start_date}') & (timestamp < '{end_date}')"
   df_range = excluded_retailers.query(query_start)
 
-  users_before = []
-  users_before = df_range.query(query_before)['user'].unique()
-  users_before = users_before[users_before != None]
+  df_range = df_range[df_range['user'].notnull()]
+  df_range = df_range[df_range['dest_user'].notnull()]
 
-  dest_users = df_range.query(query_before)['dest_user'].unique()
-  users_before = np.append(users_before, dest_users[dest_users != None])
-  users_before = users_before.unique()
+  all_users = pd.concat([df_range['user'], df_range['dest_user']]).rename('user').to_frame().drop_duplicates()
+  all_users['key'] = 1
+  x_users = pd.merge(all_users, all_users.rename(columns={'user': 'dest_user'}), on='key')[['user', 'dest_user']]
 
-  data_cleaned = []
-  
   # split the timeline before and after COVID
   ## March 25th, 2020 school closes  April 1st, 2020 roads to capital closed to town 
+  query_before = f"(timestamp >= '{start_date}') & (timestamp < '{lockdown_date}')"
+  # TODO: cut the last day
+  query_after = f"(timestamp >= '{lockdown_date}') & (timestamp < '{end_date}')"
+
   if timeline == 'before':
-    # 238 day before (8/7/2019)
-    data_cleaned =  df_range.query(query_before)
-    print("num users before" + str(data_cleaned['user'].nunique())) 
-
+    data_cleaned = df_range.query(query_before)
   elif timeline == 'after':
-    df_after =  df_range.query("timestamp >= '2020-04-01 00:00:00' ")
-
-     #  the dest users be from the same datatset too
-    df = df_after[df_after['user'].isin(users_before)]                              
-    df = df_after[df_after['dest_user'].isin(users_before)]  
-
-    df = df.merge(users_before, how = 'outer', on=['user', 'dest_user'])
-    data_cleaned = df['amount_idr'].fillna(0)
-
-    print("num users after" + str(data_cleaned['user'].nunique())) 
-
+    data_cleaned = df_range.query(query_after)
   else:
-    print("Timeline should be only 'before' or 'after' the pandemic lockdown.")
-    # TODO raise error
-  
+    raise ValueError("Timeline should be only 'before' or 'after' the pandemic lockdown.")
+
+  data_cleaned = data_cleaned.merge(x_users, on=['user', 'dest_user'], how='outer')
+  data_cleaned['amount_idr'] = data_cleaned['amount_idr'].fillna(0)
 
   # total loan for each user
   total_loan = data_cleaned.groupby(["user", "dest_user"]).agg({"amount_idr" : 'sum'}) 
-  total_loan = total_loan['amount_idr'].div(num_days)
+  total_loan['amount_idr'] = total_loan['amount_idr'].div(num_days)
   
-  # optional: exclude the maximum amount of loan (entire row)
-  total_loan = total_loan[total_loan['amount_idr'] != total_loan['amount_idr'].max()]
+  # # optional: exclude the maximum amount of loan (entire row)
+  # total_loan = total_loan[total_loan['amount_idr'] != total_loan['amount_idr'].max()]
 
   # drop the rows of retailers
   df = pd.DataFrame(total_loan).reset_index()
@@ -99,13 +89,12 @@ def make_plot(df, timeline):
     alt.Y('dest_user:N',
      title= str(count_dest) + " Destination Users",
      sort=alt.EncodingSortField(field='dest_user', order='descending')
-      # TODO making axis more static - build all the empty users to fill in 0 
      ),
     alt.Color('amount_idr:Q', 
       title="Amount Loans",
 
-      #TODO should I apply log scale? Do in panda handle log0 = 0
-      scale=alt.Scale(type='linear', nice=True),          #  TODO cheange  scale in domain 
+      scale=alt.Scale(type='symlog', nice=True),
+      condition={"test": "datum['amount_idr'] == 0", "value": "white"}
       ),
   ).save("./misc/charts/loan_credits_" + timeline +".html", scale_factor=2.0)
 
@@ -117,7 +106,7 @@ if __name__ == "__main__":
     
     get_data("before")
     
-    # get_data("after")
+    get_data("after")
 
-    # make_plot(get_data("before"), "before")
-    # make_plot(get_data("after"), "after")
+    make_plot(get_data("before"), "before")
+    make_plot(get_data("after"), "after")