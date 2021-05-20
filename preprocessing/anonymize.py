b88d487b27f0cca77c96e01b38b7d3895f464927
Matt Johnson <matt9j@cs.washington.edu>
2021-05-20 14:31:50 -0700

    preprocessing: Change anonymization order

--- preprocessing/anonymize.py
+++ preprocessing/anonymize.py
@@ -35,22 +35,6 @@ def anonymize_rare_orgs(in_path, out_path):
     infra.dask.clean_write_parquet(flows, out_path)
 
 
-def anonymize_rare_fqdns(in_path, out_path):
-    print("Anonymizing fqdns")
-    flows = infra.dask.read_parquet(in_path)
-    user_counts_per_fqdn = flows[["user", "fqdn", "bytes_up"]].groupby(["user", "fqdn"]).first().dropna().reset_index()
-    user_counts_per_fqdn = user_counts_per_fqdn.assign(user_count=1).groupby(["fqdn"]).sum()
-    user_counts_per_fqdn = user_counts_per_fqdn.drop("bytes_up", axis=1).reset_index()
-    flows_with_counts = flows.merge(user_counts_per_fqdn, on="fqdn", how="left")
-
-    flows_with_counts["fqdn"] = flows_with_counts["fqdn"].mask(
-        (flows_with_counts["user_count"] < infra.constants.MIN_K_ANON) | flows_with_counts["user_count"].isna(),
-        other="[Other Anonymized U<{}]".format(infra.constants.MIN_K_ANON),
-        )
-    flows = flows_with_counts.drop("user_count", axis=1)
-    infra.dask.clean_write_parquet(flows, out_path)
-
-
 def anonymize_rare_ips(in_path, out_path):
     print("Anonymizing destination ip addresses")
     flows = infra.dask.read_parquet(in_path)
@@ -67,17 +51,33 @@ def anonymize_rare_ips(in_path, out_path):
     infra.dask.clean_write_parquet(flows, out_path)
 
 
+def anonymize_rare_fqdns(in_path, out_path):
+    print("Anonymizing fqdns")
+    flows = infra.dask.read_parquet(in_path)
+    user_counts_per_fqdn = flows[["user", "fqdn", "bytes_up"]].groupby(["user", "fqdn"]).first().dropna().reset_index()
+    user_counts_per_fqdn = user_counts_per_fqdn.assign(user_count=1).groupby(["fqdn"]).sum()
+    user_counts_per_fqdn = user_counts_per_fqdn.drop("bytes_up", axis=1).reset_index()
+    flows_with_counts = flows.merge(user_counts_per_fqdn, on="fqdn", how="left")
+
+    flows_with_counts["fqdn"] = flows_with_counts["fqdn"].mask(
+        (flows_with_counts["user_count"] < infra.constants.MIN_K_ANON) | flows_with_counts["user_count"].isna(),
+        other="[Other Anonymized U<{}]".format(infra.constants.MIN_K_ANON),
+        )
+    flows = flows_with_counts.drop("user_count", axis=1)
+    infra.dask.clean_write_parquet(flows, out_path)
+
+
 def anonymize_all(client):
     anonymize_rare_orgs(
         "scratch/flows/typical_fqdn_org_category_local_TM_DIV_none_INDEX_start",
         "scratch/flows/typical_fqdn_org_category_local_TM_DIV_none_INDEX_none_ANON_org",
     )
-    anonymize_rare_fqdns(
-        "scratch/flows/typical_fqdn_org_category_local_TM_DIV_none_INDEX_none_ANON_org",
-        "scratch/flows/typical_fqdn_org_category_local_TM_DIV_none_INDEX_none_ANON_org_fqdn",
-    )
     anonymize_rare_ips(
-        "scratch/flows/typical_fqdn_org_category_local_TM_DIV_none_INDEX_none_ANON_org_fqdn",
+        "scratch/flows/typical_fqdn_org_category_local_TM_DIV_none_INDEX_none_ANON_org",
+        "scratch/flows/typical_fqdn_org_category_local_TM_DIV_none_INDEX_none_ANON_org_ip",
+    )
+    anonymize_rare_fqdns(
+        "scratch/flows/typical_fqdn_org_category_local_TM_DIV_none_INDEX_none_ANON_org_ip",
         "scratch/flows/typical_fqdn_org_category_local_TM_DIV_none_INDEX_none_ANON_org_fqdn_ip",
     )
 
