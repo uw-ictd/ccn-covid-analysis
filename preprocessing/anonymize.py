9dc16e6cb7cd3fb119d2eb6c1153644a368c83ab
Matt Johnson <matt9j@cs.washington.edu>
2021-05-20 14:52:12 -0700

    preprocessing: Better store data when anonymizing
    Use the dask cluster persist method to store intermediates in memory
    rather than always writing out to disk. This greatly improves
    performance on the CSE cluster where the storage is slow NFS storage
    and there is abundant local memory.

--- preprocessing/anonymize.py
+++ preprocessing/anonymize.py
@@ -6,21 +6,8 @@ import infra.pd
 import infra.platform
 
 
-def anonymize_rare_orgs(in_path, out_path):
+def anonymize_rare_orgs(client, flows, checkpoint_path=None):
     print("Anonymizing orgs")
-    flows = infra.dask.read_parquet(in_path)
-
-    # Reset the index immediately to preserve the start column across
-    # groupings. Don't reindex until after all the anonymization aggregates
-    # and groups are complete to avoid unnecessary computation.
-    flows = flows.reset_index()
-    flows = flows.astype({
-        "user": object,
-        "fqdn": object,
-        "org": object,
-        "dest_ip": object,
-    })
-
     user_counts_per_org = flows[["user", "org", "bytes_up"]].groupby(["user", "org"]).first().dropna().reset_index()
     user_counts_per_org = user_counts_per_org.assign(user_count=1).groupby(["org"]).sum()
     user_counts_per_org = user_counts_per_org.drop("bytes_up", axis=1).reset_index()
@@ -32,12 +19,16 @@ def anonymize_rare_orgs(in_path, out_path):
     )
 
     flows = flows_with_counts.drop("user_count", axis=1)
-    infra.dask.clean_write_parquet(flows, out_path)
+
+    flows = client.persist(flows)
+    if checkpoint_path is not None:
+        infra.dask.clean_write_parquet(flows, checkpoint_path)
+
+    return flows
 
 
-def anonymize_rare_ips(in_path, out_path):
+def anonymize_rare_ips(client, flows, checkpoint_path=None):
     print("Anonymizing destination ip addresses")
-    flows = infra.dask.read_parquet(in_path)
     user_counts_per_ip = flows[["user", "dest_ip", "bytes_up"]].groupby(["user", "dest_ip"]).first().dropna().reset_index()
     user_counts_per_ip = user_counts_per_ip.assign(user_count=1).groupby(["dest_ip"]).sum()
     user_counts_per_ip = user_counts_per_ip.drop("bytes_up", axis=1).reset_index()
@@ -48,12 +39,16 @@ def anonymize_rare_ips(in_path, out_path):
         other="[Other Anonymized U<{}]".format(infra.constants.MIN_K_ANON),
         )
     flows = flows_with_counts.drop("user_count", axis=1)
-    infra.dask.clean_write_parquet(flows, out_path)
+
+    flows = client.persist(flows)
+    if checkpoint_path is not None:
+        infra.dask.clean_write_parquet(flows, checkpoint_path)
+
+    return flows
 
 
-def anonymize_rare_fqdns(in_path, out_path):
+def anonymize_rare_fqdns(client, flows, checkpoint_path=None):
     print("Anonymizing fqdns")
-    flows = infra.dask.read_parquet(in_path)
     user_counts_per_fqdn = flows[["user", "fqdn", "bytes_up"]].groupby(["user", "fqdn"]).first().dropna().reset_index()
     user_counts_per_fqdn = user_counts_per_fqdn.assign(user_count=1).groupby(["fqdn"]).sum()
     user_counts_per_fqdn = user_counts_per_fqdn.drop("bytes_up", axis=1).reset_index()
@@ -64,22 +59,49 @@ def anonymize_rare_fqdns(in_path, out_path):
         other="[Other Anonymized U<{}]".format(infra.constants.MIN_K_ANON),
         )
     flows = flows_with_counts.drop("user_count", axis=1)
-    infra.dask.clean_write_parquet(flows, out_path)
+
+    flows = client.persist(flows)
+    if checkpoint_path is not None:
+        infra.dask.clean_write_parquet(flows, checkpoint_path)
+
+    return flows
 
 
 def anonymize_all(client):
-    anonymize_rare_orgs(
-        "scratch/flows/typical_fqdn_org_category_local_TM_DIV_none_INDEX_start",
-        "scratch/flows/typical_fqdn_org_category_local_TM_DIV_none_INDEX_none_ANON_org",
+    df = infra.dask.read_parquet("scratch/flows/typical_fqdn_org_category_local_TM_DIV_none_INDEX_start")
+    # Reset the index immediately to preserve the start column across
+    # groupings. Don't reindex until after all the anonymization aggregates
+    # and groups are complete to avoid unnecessary computation.
+    df = df.reset_index()
+    df = df.astype({
+        "user": object,
+        "fqdn": object,
+        "org": object,
+        "dest_ip": object,
+    })
+
+    df_org = anonymize_rare_orgs(
+        client,
+        df,
+        # checkpoint_path="scratch/flows/typical_fqdn_org_category_local_TM_DIV_none_INDEX_none_ANON_org",
     )
-    anonymize_rare_ips(
-        "scratch/flows/typical_fqdn_org_category_local_TM_DIV_none_INDEX_none_ANON_org",
-        "scratch/flows/typical_fqdn_org_category_local_TM_DIV_none_INDEX_none_ANON_org_ip",
+    del df
+
+    df_org_ip = anonymize_rare_ips(
+        client,
+        df_org,
+        checkpoint_path="scratch/flows/typical_fqdn_org_category_local_TM_DIV_none_INDEX_none_ANON_org_ip",
     )
-    anonymize_rare_fqdns(
-        "scratch/flows/typical_fqdn_org_category_local_TM_DIV_none_INDEX_none_ANON_org_ip",
-        "scratch/flows/typical_fqdn_org_category_local_TM_DIV_none_INDEX_none_ANON_org_fqdn_ip",
+    del df_org
+
+    final_df = anonymize_rare_fqdns(
+        client,
+        df_org_ip,
+        checkpoint_path="scratch/flows/typical_fqdn_org_category_local_TM_DIV_none_INDEX_none_ANON_org_fqdn_ip",
     )
+    del df_org_ip
+
+    infra.dask.clean_write_parquet(final_df, "scratch/flows/typical_fqdn_org_category_local_TM_DIV_none_INDEX_none_ANON_org_fqdn_ip")
 
 
 if __name__ == "__main__":
